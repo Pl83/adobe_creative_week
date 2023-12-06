566b07130274a4032a08edbb255e48b32a04872f
PL Sans <pierre-louis.sans@edu.devinci.fr>
2023-12-06 10:18:36 +0100

    html + css pour les sections 2 et 3, début js de la nav, ajout d'une video placeholder en atendent la vraie video, ajout du dossier video

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
+const navLinks = document.querySelectorAll('.navLink');
 
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        navLinks.forEach((link) => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
 
