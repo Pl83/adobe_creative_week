a70c482b96f669280c84b43627b35f6b516d3b2c
PL Sans <pierre-louis.sans@edu.devinci.fr>
2023-12-07 09:21:17 +0100

    remouved slider controler img

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ navLinks.forEach((link) => {
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
